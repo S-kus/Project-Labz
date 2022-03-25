8482246b8d91d400e9dd8fa5dfd482cc50a56fe9
S-kus <saumyakus@gmail.com>
2022-03-25 16:46:40 +0530

    added firebase config and login page

---
+++ project-tech/src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA3Gh71-AhUFuBGyFlr1giTBTmYFQBuvOs",
+    authDomain: "project-tech-3cbb1.firebaseapp.com",
+    projectId: "project-tech-3cbb1",
+    storageBucket: "project-tech-3cbb1.appspot.com",
+    messagingSenderId: "703216993750",
+    appId: "1:703216993750:web:ae144bc7723a7d2113d077"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
