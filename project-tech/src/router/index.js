ba67945a053cb5be802c229f307fa13810d8fbf0
S-kus <saumyakus@gmail.com>
2022-03-27 16:54:14 +0530

    added signUp functionality, navbar and projectList form

--- project-tech/src/router/index.js
+++ project-tech/src/router/index.js
@@ -1,18 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import login from '../views/auth/login.vue'
+import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
+import CreateProjectlist from '../views/projectlists/CreateProjectlist.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
     path: '/login',
-    name: 'login',
-    component: login
-  }
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/projectlists/create',
+    name: 'CreateProjectlist',
+    component: CreateProjectlist,
+    // beforeEnter: requireAuth
+  },
 ]
 
 const router = createRouter({
